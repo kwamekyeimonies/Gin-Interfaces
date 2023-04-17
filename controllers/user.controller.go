76f5f4722974137f76a5da88c09967d304abd1f0
Tenkorang Daniel <kwamekyeimonies@gmail.com>
2023-04-17 00:12:36 +0000

    controllers dev

--- controllers/user.controller.go
+++ controllers/user.controller.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/kwamekyeimonies/Gin-Interfaces/models"
 	"github.com/kwamekyeimonies/Gin-Interfaces/services"
 )
 
@@ -18,23 +19,80 @@ func New(userservice services.UserService) UserController {
 }
 
 func (u *UserController) CreateUser(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, "")
+	var user models.User
+	if err := ctx.ShouldBindJSON(&user); err != nil{
+		ctx.JSON(http.StatusBadRequest,gin.H{
+			"Error":err.Error(),
+		})
+		return
+	}
+
+	err := u.UserService.CreateUser(&user)
+	if err != nil{
+		ctx.JSON(http.StatusBadRequest,gin.H{
+			"Error":err.Error(),
+		})
+		return
+	}
+	ctx.JSON(http.StatusOK, "Success")
 }
 
 func (u *UserController) GetUser(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, "")
+	var username string = ctx.Param("name")
+	user, err := u.UserService.GetUser(&username)
+	if err != nil{
+		ctx.JSON(http.StatusBadRequest,gin.H{
+			"Error":err.Error(),
+		})
+		return
+	}
+	ctx.JSON(http.StatusOK, user)
 }
 
 func (u *UserController) GetAllUsers(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, "")
+	var user models.User
+	if err := ctx.ShouldBindJSON(&user); err != nil{
+		ctx.JSON(http.StatusBadRequest,gin.H{
+			"Error":err.Error(),
+		})
+		return
+	}
+	ctx.JSON(http.StatusOK, user)
 }
 
 func (u *UserController) UpdateUser(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, "")
+	var user models.User
+	if err := ctx.ShouldBindJSON(&user); err != nil{
+		ctx.JSON(http.StatusBadRequest,gin.H{
+			"Error":err.Error(),
+		})
+		return
+	}
+	err := u.UserService.UpdateUser(&user)
+	if err != nil{
+		ctx.JSON(http.StatusBadGateway,gin.H{
+			"Error":err.Error(),
+		})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{
+		"Message":"User updated successfully",
+		"User":user,
+	})
 }
 
 func (u *UserController) DeleteUser(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, "")
+	var username string = ctx.Param("name")
+	err := u.UserService.DeleteUser(&username)
+	if err != nil{
+		ctx.JSON(http.StatusBadGateway,gin.H{
+			"Error":err.Error(),
+		})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{
+		"Message":"Deleted Successfully",
+	})
 }
 
 func (ux *UserController) RegisterUserRoutes(rg *gin.RouterGroup){
